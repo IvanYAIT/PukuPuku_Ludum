107aeba8a3c87f4934671c85464cec161da28bdc
IvanYAIT <YAkushikia20@st.ithub.ru>
2023-10-02 15:43:38 +0300

    add art to main menu

---
+++ PukuPuku(LudumDare54)/Assets/_Source/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button playBtn;
+        void Start()
+        {
+            playBtn.onClick.AddListener(Play);
+        }
+
+        private void Play() =>
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        private void OnDestroy()
+        {
+            playBtn.onClick.RemoveListener(Play);
+        }
+    }
+}
